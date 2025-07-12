dc11d315245b98f4f20bf89ac7266c9ad0221770
jeffbozu <104255821+jeffbozu@users.noreply.github.com>
2025-07-12 04:59:03 +0200

    Use Cupertino icons to avoid missing Material glyphs

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -543,7 +544,7 @@ class _HomePageState extends State<HomePage> {
                               _emergencyActive ? Colors.grey : const Color(0xFFE62144),
                           minimumSize: const Size(40, 40),
                         ),
-                        child: const Icon(Icons.remove, color: Colors.white),
+                        child: const Icon(CupertinoIcons.minus, color: Colors.white),
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -562,7 +563,7 @@ class _HomePageState extends State<HomePage> {
                               _emergencyActive ? Colors.grey : const Color(0xFFE62144),
                           minimumSize: const Size(40, 40),
                         ),
-                        child: const Icon(Icons.add, color: Colors.white),
+                        child: const Icon(CupertinoIcons.add, color: Colors.white),
                       ),
                     ],
                   ),

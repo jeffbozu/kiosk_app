0d7e0da04ecdaa44f96c4eeee9364b9f11e8ccdf
jeffbozu <104255821+jeffbozu@users.noreply.github.com>
2025-08-01 07:59:54 +0200

    Update api_config.dart

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,4 +1,4 @@
 /// Global API configuration
 /// Default API base URL for the mock backend
-const String defaultApiBaseUrl = 'https://tariff2.onrender.com';
+const String defaultApiBaseUrl = 'https://mock-mowiz.onrender.com';
 
